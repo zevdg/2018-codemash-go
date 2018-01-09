67e1f36c6661a5be37bc8db1ea5bce426b32fa60
Zev Goldstein <Zev.Goldstein@gmail.com>
2018-01-09 13:54:12 -0500

    add solution to lab 1

--- labs/01/ipsum/ipsum.go
+++ labs/01/ipsum/ipsum.go
@@ -6,7 +6,11 @@ import (
 	"time"
 )
 
-var wordbank = []string{}
+var wordbank = []string{
+	"foo",
+	"bar",
+	"baz",
+}
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
@@ -14,13 +18,32 @@ func main() {
 }
 
 func generateIpsum(wordCount, sentenceLength int) string {
-
+	ipsum := ""
+	for wordsLeft := wordCount; wordsLeft > 0; wordsLeft -= sentenceLength {
+		numWords := sentenceLength
+		if wordsLeft < numWords {
+			numWords = wordsLeft
+		}
+		if ipsum != "" {
+			ipsum += "  "
+		}
+		ipsum += generateSentence(numWords)
+	}
+	return ipsum
 }
 
-func generateSentence(wordCount int) {
-
+func generateSentence(wordCount int) string {
+	ipsum := ""
+	for i := 0; i < wordCount; i++ {
+		if ipsum != "" {
+			ipsum += " "
+		}
+		ipsum += getWord()
+	}
+	return ipsum + "."
 }
 
 func getWord() string {
-
+	index := rand.Intn(len(wordbank))
+	return wordbank[index]
 }
